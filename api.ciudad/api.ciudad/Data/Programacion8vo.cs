dc5a6a2d331ffc3feae4f29acef8bc5f8f1f93db
cuevasjorge <jorge01cuevaszarate@gmail.com>
2022-10-17 20:47:06 +0200

    Se agrego

---
+++ api.ciudad/api.ciudad/Data/Programacion8vo.cs
@@ -0,0 +1,15 @@
+﻿using api.ciudad.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.ciudad.Data
+{
+    public class Programacion8vo : DbContext
+    {
+        public Programacion8vo(DbContextOptions<Programacion8vo> options) : base(options)
+        {
+
+
+        }
+        public DbSet<Clientes> Clientes => Set<Clientes>();
+    }
+}
---
+++ api.ciudad/api.ciudad/Models/Clientes.cs
@@ -0,0 +1,10 @@
+﻿namespace api.ciudad.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public string Ciudad { get; set; }
+        public string Estado { get; set; }
+       
+    }
+}
